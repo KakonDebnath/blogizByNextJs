fbc4578a923098b7fed4945ae480d1ba8e8cc8fe
Kakon Debnath <kakondebnath96@gmail.com>
2024-03-10 20:26:31 +0600

    get preBuilt details page by generateStaticParams fn

--- src/app/blogs/[blogId]/page.tsx
+++ src/app/blogs/[blogId]/page.tsx
@@ -1,9 +1,19 @@
 import BlogDetails from '@/components/ui/BlogDetails';
 import { TBlogId, TBlog } from '@/types';
 
+export const generateStaticParams = async () => {
+  const res = await fetch(`http://localhost:5000/blogs`);
+  const blogs = await res.json();
+  return blogs.slice(0, 3).map((blog: TBlog) => ({
+    blogId: blog.id,
+  }));
+};
+
 const DynamicBlogPage = async ({ params }: TBlogId) => {
   const blogId = params.blogId;
-  const res = await fetch(`http://localhost:5000/blogs/${blogId}`);
+  const res = await fetch(`http://localhost:5000/blogs/${blogId}`, {
+    cache: 'no-store',
+  });
   const blog: TBlog = await res.json();
   return (
     <div className="mt-10">
